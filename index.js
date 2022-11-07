6ecc1180767688c4dd2cb126f550b53cf2d083cb
pund shweta balasaheb <113433586+shwetapund@users.noreply.github.com>
2022-11-07 05:19:44 -0800

    index file added

---
+++ index.js
@@ -0,0 +1,21 @@
+/*const arr = ["satyam","rupal","sakshi"]
+console.log(arr)
+arr.pop()
+console.log(arr)
+*/
+
+/*const arr = ["satyam","rupal","sakshi"]
+console.log(arr.length)
+
+arr.push("noodles")
+
+console.log(arr.length)
+*/
+
+arr = ["shivam","shreya","shyam"]
+
+console.log(arr)
+
+arr.reverse()
+
+console.log(arr)
